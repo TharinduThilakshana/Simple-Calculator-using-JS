bfd396b6b995576b81990532c4b20f09d1963ac7
Tharindu Thilakshana <91044135+TharinduThilakshana@users.noreply.github.com>
2024-04-08 12:36:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+///     JAVASCRIPT CODE FOR CALCULATOR
+// BITS 24' ACICTS!
+
+const display = document.getElementById("display")
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+
+
+
+
+
+
+
+
+
+
